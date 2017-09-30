8cd6cda5c6982b904c0f5aed243a40ec4b651ee9
Peter Osterlund <peterosterlund2@gmail.com>
2017-09-30 06:52:50 +0000

    Update texel to version 1.07a37.
    git-svn-id: file:///home/petero/vc/svnrepos/chess/TexelChessEngine@41 099bbecd-d45e-4991-adbe-d216ea0d3dce

--- jni/texel/Android.mk
+++ jni/texel/Android.mk
@@ -1,12 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
-TX_SRC	:= bitBoard.cpp book.cpp computerPlayer.cpp enginecontrol.cpp endGameEval.cpp evaluate.cpp \
+TX_SRC	:= bitBoard.cpp book.cpp cluster.cpp clustertt.cpp computerPlayer.cpp \
+	   enginecontrol.cpp endGameEval.cpp evaluate.cpp \
 	   game.cpp history.cpp humanPlayer.cpp killerTable.cpp kpkTable.cpp \
-	   krkpTable.cpp krpkrTable.cpp material.cpp move.cpp moveGen.cpp numa.cpp parameters.cpp \
-	   parallel.cpp piece.cpp position.cpp search.cpp tbprobe.cpp texel.cpp textio.cpp \
-	   polyglot.cpp transpositionTable.cpp tbgen.cpp treeLogger.cpp tuigame.cpp uciprotocol.cpp \
-	   util/logger.cpp util/random.cpp util/timeUtil.cpp util/util.cpp \
-	   syzygy/rtb-probe.cpp
+	   krkpTable.cpp krpkrTable.cpp largePageAlloc.cpp material.cpp move.cpp \
+	   moveGen.cpp numa.cpp parameters.cpp parallel.cpp piece.cpp position.cpp \
+	   search.cpp tbprobe.cpp texel.cpp textio.cpp polyglot.cpp transpositionTable.cpp \
+	   tbgen.cpp treeLogger.cpp tuigame.cpp uciprotocol.cpp util/logger.cpp \
+	   util/random.cpp util/timeUtil.cpp util/util.cpp syzygy/rtb-probe.cpp
 
 TX_SRC_GTB := gtb/compression/lzma/Lzma86Dec.c gtb/compression/lzma/LzFind.c \
 	      gtb/compression/lzma/Lzma86Enc.c gtb/compression/lzma/LzmaDec.c \
@@ -23,5 +24,6 @@ LOCAL_SRC_FILES := $(TX_SRC) $(TX_SRC_GTB)
 LOCAL_CFLAGS    := -O3 -fPIE $(TX_INC) -DHAS_CTZ -DHAS_PREFETCH -DHAS_RT
 LOCAL_CPPFLAGS  := -std=c++11
 LOCAL_LDFLAGS	+= -fPIE -pie
+LOCAL_LDLIBS	+= -latomic
 LOCAL_CPP_FEATURES := rtti exceptions
 include $(BUILD_EXECUTABLE)
